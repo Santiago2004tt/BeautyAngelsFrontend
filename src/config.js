7c22e1f7c0f0f6f598783e2c4212c60d1d310233
Santiago2004tt <santiago.sepulvedab@uqvirtual.edu.co>
2025-11-10 13:08:44 -0500

    nueva api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const API = "https://beautyangelsbackend.onrender.com";
+export const API_2 = "https://beautyangelsbackend.onrender.com";
 export const API_1 = "http://127.0.0.1:8000";
+export const API = "http://18.222.144.90:8000"
